4d7330bfce447d119aece2c7c59b45a44cb83803
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-18 14:25:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Ethan Lawrence
+# Sept 18
+# simple Calc
+
+# Input
+num1 = int(input('Please enter a number:    '))
+num2 = int(input('Please enter a number:    '))
+# Process
+total = num1 + num2
+# total = num1 - num2   Subtract
+# total = num1 * num2   Multiply
+# total = num1 / num2   Divide
+# total = num1 ** num2  Exponetiation
+
+# Output
+print('The sum of the two numbers is ' + str(total) +'.')
